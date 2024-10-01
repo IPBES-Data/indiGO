49154a52ee8c1e2e1a4ffde37403337472c05078
hetzerj <jessica.hetzer@senckenberg.de>
2024-10-01 10:45:04 +0200

    Renaming functions - renaming load_indicatorSummary to indi_summary
    ToDO: Currently file is given in csv and and read in by read.csv. Should be text file instead

--- R/indi_summary.R
+++ R/indi_summary.R
@@ -2,15 +2,15 @@
 #'
 #' This function loads a detailed overview of the metadata for indicators available in the package. The summary includes key information such as the indicator name, data sources, time span covered, EBV class, and other relevant attributes.
 #'
-#' @return A data frame containing comprehensive metadata for the available indicators, including details on the indicator's name, data sources, time span, EBV class, and other relevant attributes.
+#' @return A data frame containing comprehensive metadata for the available indicators of the package.
 #' @examples
 #' # Load the comprehensive metadata for the indicators
-#' Tab1.dat <- load_indicatorSummary()
+#' Tab1.dat <- indi_summary()
 #' head(Tab1.dat)
 #'
 #' @references IPBES (2019). *Global Assessment Chapter 2.2 – Supplementary Material: Indicators of Status & Trends in Nature*.
 #'
-load_indicatorSummary <- function() {
+indi_summary <- function() {
   # Define the file path for the metadata summary
   data_path <- system.file("Metadata", "SOD_Table1.csv", package = "indiGO")
 
