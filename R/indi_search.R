9c776bd422c002caaa40e1f66c35ba21e01b2259
hetzerj <jessica.hetzer@senckenberg.de>
2024-10-01 10:40:29 +0200

    Renaming functions - Renaming list_indicators_by_searchword to indi_search

--- R/indi_search.R
+++ R/indi_search.R
@@ -1,15 +1,15 @@
 #' List Filenames Containing a Specific Search Word
 #'
-#' This function searches for and returns filenames in the data directory set by the option `indicatorDataPath` that contain a specific search word.
+#' This function searches for and returns indicator names in the data directory set by the option `indicatorDataPath` that contain a specific search word.
 #'
-#' @param searchWord A character string representing the word to search for in filenames.
-#' @return A character vector of filenames containing the search word, or a message if no files are found.
+#' @param searchWord A character string representing the pattern to search for in file names.
+#' @return A character vector of file names containing the search pattern. If no files are found a message appears in the console.
 #' @examples
 #' # List all files in the directory containing 'biodiversity' in the name
-#' searchResults <- list_indicators_by_searchword("biodiversity")
+#' searchResults <- indi_search("biodiversity")
 #' print(searchResults)
 #'
-list_indicators_by_searchword <- function(searchWord) {
+indi_search <- function(searchWord) {
 
   # List all files in the directory that match the search word
   fileList <- list.files(path = getOption("indicatorDataPath"), pattern = searchWord, ignore.case = TRUE)
